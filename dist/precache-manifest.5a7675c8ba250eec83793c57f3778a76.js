af35b012ded1ecd2317e6f78336376a54c4128b5
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-04-10 10:19:28 +0100

    build app

---
+++ dist/precache-manifest.5a7675c8ba250eec83793c57f3778a76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11eafe3c50b14c48458c",
+    "url": "/css/about.f841d6fe.css"
+  },
+  {
+    "revision": "d81d341e5b4c2d4f5fc0",
+    "url": "/css/app.a2941d9c.css"
+  },
+  {
+    "revision": "70cd7ed3ced1c290ffc7",
+    "url": "/css/chunk-70e99f35.8650aa77.css"
+  },
+  {
+    "revision": "210d19ae5c8423ca2418",
+    "url": "/css/chunk-b628c016.8650aa77.css"
+  },
+  {
+    "revision": "44323591257216ce8b1a",
+    "url": "/css/chunk-vendors.9faae025.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "f39a110e7ded098c6895949d94b689ae",
+    "url": "/img/new.f39a110e.svg"
+  },
+  {
+    "revision": "4c0cb60e0a951303668d9eae0238a3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11eafe3c50b14c48458c",
+    "url": "/js/about.395a1e43.js"
+  },
+  {
+    "revision": "d81d341e5b4c2d4f5fc0",
+    "url": "/js/app.023cad16.js"
+  },
+  {
+    "revision": "70cd7ed3ced1c290ffc7",
+    "url": "/js/chunk-70e99f35.8bd355f6.js"
+  },
+  {
+    "revision": "210d19ae5c8423ca2418",
+    "url": "/js/chunk-b628c016.6d30e62a.js"
+  },
+  {
+    "revision": "44323591257216ce8b1a",
+    "url": "/js/chunk-vendors.4a39f7b4.js"
+  },
+  {
+    "revision": "21ffb7c2d41a3503b7cb853a5ced72f6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
